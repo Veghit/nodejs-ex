aec75114e916f29c69943d9d3cce5cdce8aaeeac
Unknown <itay.vegh@gmail.com>
2018-01-19 14:45:35 +0200

    changed port to 8080

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const DB = require('./server/data/DB')
 
 DB.connectDB()
     .then(_=> {
-        WebServer.listenOnPort('3000')
-        console.log('listening on port 3000')
+        WebServer.listenOnPort('8080')
+        console.log('listening on port 8080')
     }).catch(err => console.log(err))
 
 
